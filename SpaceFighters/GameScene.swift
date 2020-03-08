c83ec0e4be2e573636e414a4ca548e296462cef8
davalcato <davalcato@yahoo.com>
2020-03-08 11:15:39 -0700

    first commit

---
+++ SpaceFighters/GameScene.swift
@@ -0,0 +1,28 @@
+//
+//  GameScene.swift
+//  SpaceFighters
+//
+//  Created by Daval Cato on 3/8/20.
+//  Copyright © 2020 Daval Cato. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    
+    override func didMove(to view: SKView) {
+        
+       
+    }
+    
+    
+   
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
