81c0fd8858edcfd5bfc1b6d66be8ca3a4d066946
davalcato <davalcato@yahoo.com>
2020-03-08 12:21:30 -0700

    added a computed property didSet {             scoreLabel.text = "Score: \(score)" to the GameScene file

--- SpaceFighters/GameScene.swift
+++ SpaceFighters/GameScene.swift
@@ -9,18 +9,37 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var starfield:SKEmitterNode!
     var player:SKSpriteNode!
     
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+            
+        }
+        
+    }
+    
     override func didMove(to view: SKView) {
+      
         starfield = SKEmitterNode(fileNamed: "Starfield")
         starfield.position = CGPoint(x: 0, y: 1472)
         starfield.advanceSimulationTime(10)
         self.addChild(starfield)
         
         starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "shuttle")
+//        player.position = CGPoint(x: self.frame.size.width / 2, y: player.size.height / 2 + 20)
+        
+        player.size = CGSize(width: self.player.size.width*2, height: self.player.size.height*2)
+        
+        self.addChild(player)
+        self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        self.physicsWorld.contactDelegate = self
        
     }
     
