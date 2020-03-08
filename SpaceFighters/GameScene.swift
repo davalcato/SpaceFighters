e065bb333b8b54d1225ff3ac2373dede8efaf873
davalcato <davalcato@yahoo.com>
2020-03-08 13:25:53 -0700

    added a possibleAliens = GKRandomSource.sharedRandom to the func addAlien in the GameScene

--- SpaceFighters/GameScene.swift
+++ SpaceFighters/GameScene.swift
@@ -19,10 +19,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         didSet {
             scoreLabel.text = "Score: \(score)"
         }
-        
     }
     
     var gameTimer:Timer!
+    
+    var possibleAliens = ["alien", "alien2", "alien3"]
     
     override func didMove(to view: SKView) {
       
@@ -58,7 +59,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     @objc func addAlien () {
-        
+        possibleAliens = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: possibleAliens) as!
+        [String]
         
         
     }
