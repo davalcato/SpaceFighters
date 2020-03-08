cc29c81ac56fb1c38b829eb9acf2bbeb7bc51eed
davalcato <davalcato@yahoo.com>
2020-03-08 11:21:50 -0700

    added the var starfield:SKEmitterNode!  & var player:SKSpriteNode! to the GameScene file

--- SpaceFighters/GameScene.swift
+++ SpaceFighters/GameScene.swift
@@ -11,16 +11,14 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    
+    var starfield:SKEmitterNode!
+    var player:SKSpriteNode!
     
     override func didMove(to view: SKView) {
-        
+        starfield = SKEmitterNode(fileNamed: "Starfield")
+        starfield.position = CGPoint(x: 0, y: 1472)
        
     }
-    
-    
-   
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
