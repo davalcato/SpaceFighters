145d5c5ab3387d0c927ef93c814b17d4eec04c2d
davalcato <davalcato@yahoo.com>
2020-03-08 17:33:09 -0700

    added the func didSimulatePhysics and applied player.position.x += xAcceleration * 50

--- SpaceFighters/GameScene.swift
+++ SpaceFighters/GameScene.swift
@@ -8,6 +8,7 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
@@ -27,6 +28,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let alienCategory:UInt32 = 0x1 << 1
     let photoTorpedoCategory:UInt32 = 0x1 << 0
+    
+    let motionManger = CMMotionManager()
+    var xAcceleration:CGFloat = 0
+    
     
     override func didMove(to view: SKView) {
       
@@ -58,6 +63,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.addChild(scoreLabel)
        
         gameTimer = Timer.scheduledTimer(timeInterval: 0.75, target: self, selector: #selector(addAlien), userInfo: nil, repeats: true)
+        
+        motionManger.accelerometerUpdateInterval = 0.2
+        motionManger.startAccelerometerUpdates(to: OperationQueue.current!) { (data:CMAccelerometerData?, error:Error?) in
+            if let accelerometerData = data {
+                let acceleration = accelerometerData.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.75 + self.xAcceleration * 0.25
+                
+            }
+        }
         
     }
     
@@ -158,6 +172,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         score += 5
+    }
+    
+    override func didSimulatePhysics() {
+        player.position.x += xAcceleration * 50
+        
+        if player.position.x < -20 {
+            player.position = CGPoint(x: self.size.width + 20, y: player.position.y)
+            
+        }else if player.position.x > self.size.width + 20 {
+            
+            player.position = CGPoint(x: -20, y: player.position.y)
+        }
+        
     }
     
     override func update(_ currentTime: TimeInterval) {
