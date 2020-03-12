85dfeb07066dc94e98dc99d75f4915bd74748410
davalcato <davalcato@yahoo.com>
2020-03-12 16:47:45 -0700

    added the if let scene = SKScene(fileNamed: "MenuScene") in the GameViewController to cause the transition of screens

--- SpaceFighters/MenuScene.swift
+++ SpaceFighters/MenuScene.swift
@@ -2,11 +2,10 @@
 //  MenuScene.swift
 //  SpaceFighters
 //
-//  Created by Daval Cato on 3/11/20.
+//  Created by Daval Cato on 3/12/20.
 //  Copyright © 2020 Daval Cato. All rights reserved.
 //
 
-import UIKit
 import SpriteKit
 
 class MenuScene: SKScene {
@@ -17,32 +16,31 @@ class MenuScene: SKScene {
     var difficultyLabelNode:SKLabelNode!
     
     override func didMove(to view: SKView) {
-        starfield = self.childNode(withName: "starfield") as! SKEmitterNode
+        
+        starfield = self.childNode(withName: "starfield") as? SKEmitterNode
         starfield.advanceSimulationTime(10)
         
-        newGameButtonNode = self.childNode(withName: "newGameButton") as! SKSpriteNode
-        difficultyButtonNode = self.childNode(withName: "difficultyButton") as! SKSpriteNode
+        newGameButtonNode = self.childNode(withName: "newGameButton") as? SKSpriteNode
+        difficultyButtonNode = (self.childNode(withName: "difficultyButton") as! SKSpriteNode)
         
         difficultyButtonNode.texture = SKTexture(imageNamed: "difficultyButton")
-        difficultyLabelNode = self.childNode(withName: "difficultyLabel") as! SKLabelNode
+        difficultyLabelNode = self.childNode(withName: "difficultyLabel") as? SKLabelNode
         
         
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first
+        
         if let location = touch?.location(in: self) {
             let nodesArray = self.nodes(at: location)
             
             if nodesArray.first?.name == "newGameButton" {
                 let transition = SKTransition.flipHorizontal(withDuration: 0.5)
                 let gameScene = GameScene(size: self.size)
-                
                 self.view?.presentScene(gameScene, transition: transition)
                 
-                
             }
-            
         }
     }
 }
