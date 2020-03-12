8ac27e91181f86cf523de1bac00fd2c705202cd6
davalcato <davalcato@yahoo.com>
2020-03-11 19:18:02 -0700

    added the MenuScene file created a MenuScene class then I added the     var starfield:SKEmitterNode!     var newGameButtonNode:SKSpriteNode!     var difficultyButtonNode:SKSpriteNode!     var difficultyLabelNode:SKLabelNode!

---
+++ SpaceFighters/MenuScene.swift
@@ -0,0 +1,21 @@
+//
+//  MenuScene.swift
+//  SpaceFighters
+//
+//  Created by Daval Cato on 3/11/20.
+//  Copyright © 2020 Daval Cato. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MenuScene: SKScene {
+
+    var starfield:SKEmitterNode!
+    var newGameButtonNode:SKSpriteNode!
+    var difficultyButtonNode:SKSpriteNode!
+    var difficultyLabelNode:SKLabelNode!
+    
+    
+    
+}
