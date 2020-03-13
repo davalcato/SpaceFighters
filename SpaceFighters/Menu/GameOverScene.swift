a5d34ee5e086a11441cc2df45dc2f732908c25f2
davalcato <davalcato@yahoo.com>
2020-03-13 12:43:17 -0700

    added the scoreLabel = self.childNode(withName: "scoreLabel") and the touches began for the newGameButton on the GameOverScene

--- SpaceFighters/Menu/GameOverScene.swift
+++ SpaceFighters/Menu/GameOverScene.swift
@@ -10,6 +10,27 @@ import SpriteKit
 
 class GameOverScene: SKScene {
     
+    var score:Int = 0
+    
+    var scoreLabel:SKLabelNode!
+    var newGameButtonNode:SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        scoreLabel = self.childNode(withName: "scoreLabel") as! SKLabelNode
+        scoreLabel.text = "\(score)"
+        
+        newGameButtonNode = self.childNode(withName: "newGameButton") as! SKSpriteNode
+        newGameButtonNode.texture = SKTexture(imageNamed: "newGameButton")
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch
+    }
+    
+    
+    
+    
     
 
 }
